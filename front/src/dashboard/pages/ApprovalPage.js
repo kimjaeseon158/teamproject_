41450b4914f4bdb76cbb2c6061cfd865baeccca5
rlawotjs158 <rlawotjs158@naver.com>
2025-08-19 21:12:25 +0900

    승인 페이지 시간및 장소 추가

--- front/src/dashboard/pages/ApprovalPage.js
+++ front/src/dashboard/pages/ApprovalPage.js
@@ -15,6 +15,8 @@ import {
   ModalHeader,
   ModalFooter,
   ModalBody,
+  Tag,
+  Flex,
   useDisclosure,
 } from "@chakra-ui/react";
 import { employees } from "../js/employeeData";
@@ -57,27 +59,18 @@ export default function ApprovePage() {
               <Td>{emp.employeeNumber}</Td>
               <Td>{emp.name}</Td>
               <Td>
-                <span
-                  style={{
-                    padding: "4px 8px",
-                    borderRadius: "8px",
-                    fontWeight: "600",
-                    color:
-                      emp.status === "승인"
-                        ? "#276749"
-                        : emp.status === "거절"
-                        ? "#9b2c2c"
-                        : "#744210",
-                    backgroundColor:
-                      emp.status === "승인"
-                        ? "#c6f6d5"
-                        : emp.status === "거절"
-                        ? "#fed7d7"
-                        : "#fefcbf",
-                  }}
+                <Tag
+                  size="sm"
+                  colorScheme={
+                    emp.status === "승인"
+                      ? "green"
+                      : emp.status === "거절"
+                      ? "red"
+                      : "yellow"
+                  }
                 >
                   {emp.status}
-                </span>
+                </Tag>
               </Td>
               <Td>{emp.date}</Td>
             </Tr>
@@ -87,15 +80,65 @@ export default function ApprovePage() {
 
       {/* 상세 모달 */}
       {selectedEmployee && (
-        <Modal isOpen={isOpen} onClose={onClose}>
+        <Modal isOpen={isOpen} onClose={onClose} size="lg">
           <ModalOverlay />
           <ModalContent>
             <ModalHeader>사원 상세 정보</ModalHeader>
             <ModalBody>
-              <p>사번: {selectedEmployee.employeeNumber}</p>
-              <p>이름: {selectedEmployee.name}</p>
-              <p>상태: {selectedEmployee.status}</p>
-              <p>신청일: {selectedEmployee.date}</p>
+              <Box
+                border="1px solid #ddd"
+                borderRadius="8px"
+                p={4}
+                mb={4}
+                bg="#f9f9f9"
+              >
+                <Flex mb={2}>
+                  <Box flex="1"><strong>사번:</strong> {selectedEmployee.employeeNumber}</Box>
+                  <Box flex="1"><strong>이름:</strong> {selectedEmployee.name}</Box>
+                </Flex>
+                <Flex mb={2}>
+                  <Box flex="1">
+                    <strong>상태:</strong>{" "}
+                    <Tag
+                      size="sm"
+                      colorScheme={
+                        selectedEmployee.status === "승인"
+                          ? "green"
+                          : selectedEmployee.status === "거절"
+                          ? "red"
+                          : "yellow"
+                      }
+                    >
+                      {selectedEmployee.status}
+                    </Tag>
+                  </Box>
+                  <Box flex="1"><strong>신청일:</strong> {selectedEmployee.date}</Box>
+                </Flex>
+                <Flex mb={2}>
+                  <Box flex="1"><strong>작업시간:</strong> {selectedEmployee.workTime}</Box>
+                  <Box flex="1"><strong>업체명/위치:</strong> {selectedEmployee.location}</Box>
+                </Flex>
+                <Flex mb={2}>
+                  <Box flex="1">
+                    <strong>잔업:</strong>{" "}
+                    {selectedEmployee.overtimeChecked
+                      ? selectedEmployee.overtimeDuration
+                      : "없음"}
+                  </Box>
+                  <Box flex="1">
+                    <strong>중식:</strong>{" "}
+                    {selectedEmployee.lunchChecked
+                      ? selectedEmployee.lunchDuration
+                      : "없음"}
+                  </Box>
+                </Flex>
+                <Box flex="1">
+                  <strong>특근:</strong>{" "}
+                  {selectedEmployee.specialWorkChecked
+                    ? selectedEmployee.specialWorkDuration
+                    : "없음"}
+                </Box>
+              </Box>
             </ModalBody>
             <ModalFooter>
               <Button colorScheme="green" mr={3}>

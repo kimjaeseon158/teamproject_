4cc9ac5afb18e534baf9f5c09db47c4f1163f98f
rlawotjs158 <rlawotjs158@naver.com>
2026-02-10 23:09:38 +0900

    admin login 외부 테스트

--- front/src/login/api/loginApi.js
+++ front/src/login/api/loginApi.js
@@ -1,5 +1,5 @@
 // src/login/api/loginApi.js
-
+const API_BASE_URL = "https://teamproject-v2qg.onrender.com";
 export const adminLoginAPI = async (id, password, admin_code) => {
   const response = await fetch("/api/check_admin_login/", {
     method: "POST",
@@ -12,7 +12,7 @@ export const adminLoginAPI = async (id, password, admin_code) => {
 };
 
 export const userLoginAPI = async (id, password) => {
-  const response = await fetch("/api/check_user_login/", {
+  const response = await fetch(`${API_BASE_URL}/api/check_user_login/`, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     credentials: "include",

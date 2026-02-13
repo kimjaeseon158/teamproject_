b42208b6d7cf6d873eff22b1fdb001c793146080
rlawotjs158 <rlawotjs158@naver.com>
2026-02-13 20:46:41 +0900

    front mobile & tablet

--- front/src/user_Calendar/components/CalendarSidebar.js
+++ front/src/user_Calendar/components/CalendarSidebar.js
@@ -1,10 +1,16 @@
-import { Box } from "@chakra-ui/react";
+import { Box, IconButton } from "@chakra-ui/react";
+import { CloseIcon } from "@chakra-ui/icons";
 import Option from "../components/option";
 
-export default function CalendarSidebar({ userName, selectedDate }) {
+export default function CalendarSidebar({
+  userName,
+  selectedDate,
+  onClose,
+}) {
   return (
     <Box
-      width="350px"
+      w="100%"
+      h="100%"
       bg="#1c1c1e"
       color="white"
       p="20px"
@@ -12,11 +18,28 @@ export default function CalendarSidebar({ userName, selectedDate }) {
       flexDirection="column"
       gap="20px"
     >
-      <Box fontSize="24px" fontWeight="800">
-        {userName}님
+      {/* 🔥 상단 헤더 */}
+      <Box
+        display="flex"
+        justifyContent="space-between"
+        alignItems="center"
+      >
+        <Box fontSize="20px" fontWeight="800">
+          {userName}님
+        </Box>
+
+        <IconButton
+          icon={<CloseIcon />}
+          size="sm"
+          variant="ghost"
+          color="white"
+          _hover={{ bg: "whiteAlpha.200" }}
+          onClick={onClose}
+        />
       </Box>
 
-      <Box bg="#2c2c2e" p="14px" borderRadius="10px">
+      {/* 🔥 날짜 상세 */}
+      <Box bg="#2c2c2e" p="14px" borderRadius="12px">
         <Option selectedDate={selectedDate} />
       </Box>
     </Box>
---
+++ front/src/user_Calendar/layout/CalendarDesktopLayout.js
@@ -0,0 +1,55 @@
+import { Box } from "@chakra-ui/react";
+
+import CalendarSidebar from "../components/CalendarSidebar";
+import CalendarHeader from "../components/CalendarHeader";
+import CalendarView from "../components/CalendarView";
+
+export default function CalendarDesktopLayout({
+  userName,
+  userUuid,
+  calendar,
+  monthPickerYear,
+  setMonthPickerYear,
+  goToday,
+  goToDate,
+  calendarTitle,
+  setCalendarTitle,
+}) {
+  return (
+    <Box display="flex" height="100vh" overflow="hidden">
+
+      {/* 왼쪽 사이드바 */}
+      <Box w="18%" minW="250px">
+        <CalendarSidebar
+          userName={userName}
+          selectedDate={calendar.selectedDate}
+        />
+      </Box>
+
+      {/* 오른쪽 메인 영역 */}
+      <Box flex="1" px="20px" pt="30px" display="flex" flexDirection="column">
+
+        {/* 헤더 */}
+        <CalendarHeader
+          userUuid={userUuid}
+          monthPickerYear={monthPickerYear}
+          setMonthPickerYear={setMonthPickerYear}
+          goToday={goToday}
+          goToDate={goToDate}
+          calendarTitle={calendarTitle}
+        />
+
+        {/* 캘린더 영역 */}
+        <Box flex="1" mt={2} overflow="hidden">
+          <CalendarView
+            events={[]}
+            selectedDate={calendar.selectedDate}
+            onDateClick={calendar.handleDateClick}
+            onTitleChange={setCalendarTitle}
+          />
+        </Box>
+
+      </Box>
+    </Box>
+  );
+}

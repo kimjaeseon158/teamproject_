7ed833929d41a7accbfda74ce2b418a1b2ee1b0a
rlawotjs158 <rlawotjs158@naver.com>
2026-02-24 23:38:44 +0900

    filtering api 수정

--- front/src/feactures/admin/work_palce/api/userWorkplace_list.js
+++ front/src/feactures/admin/work_palce/api/userWorkplace_list.js
@@ -4,9 +4,13 @@ export async function getWorkPlaceList(params = {}, toast) {
   try {
     // 🔥 값 있는 것만 query로 생성
     const query = new URLSearchParams(params).toString();
+    const baseUrl =
+      params.user_name || params.work_place
+        ? "/api/work_place_rate_list_filtering/"
+        : "/api/work_place_rate_list_create/";
 
     const res = await fetchWithAuth(
-      `/api/work_place_rate_list_create/${query ? `?${query}` : ""}`,
+      `${baseUrl}${query ? `?${query}` : ""}`,
       { method: "GET" },
       { toast }
     );

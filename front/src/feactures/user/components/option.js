58b3e47e96b198c4e86a742c16e5118a862132ba
rlawotjs158 <rlawotjs158@naver.com>
2026-03-04 21:43:08 +0900

    ESLint 수정

--- front/src/feactures/user/components/option.js
+++ front/src/feactures/user/components/option.js
@@ -77,8 +77,11 @@ const Option = ({ selectedDate }) => {
     if (extraEnabled && extraWorks.length === 0) {
       setExtraWorks([{ type: "", start: "", finish: "", duration: "" }]);
     }
-    if (!extraEnabled) setExtraWorks([]);
-  }, [extraEnabled]);
+
+    if (!extraEnabled && extraWorks.length > 0) {
+      setExtraWorks([]);
+    }
+  }, [extraEnabled, extraWorks.length]);
 
   const updateExtraWork = (idx, patch) => {
     setExtraWorks((prev) =>

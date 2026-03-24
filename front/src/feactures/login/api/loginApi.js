2cb72f5748442b2049bfd1f099916808cddcf707
rlawotjs158 <rlawotjs158@naver.com>
2026-03-24 23:25:46 +0900

    2차

--- front/src/feactures/login/api/loginApi.js
+++ front/src/feactures/login/api/loginApi.js
@@ -3,7 +3,7 @@ const API_BASE = process.env.BACK_APP_API_BASE_URL;
 
 
 export const adminLoginAPI = async (id, password, admin_code) => {
-  const response = await fetch(`${API_BASE}/api/check_admin_login/`, {
+  const response = await fetch(`/api/check_admin_login/`, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     credentials: "include",

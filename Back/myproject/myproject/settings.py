e81028dcb53e5c085d514f7fc2c5d7a3c006bd08
leetjdgkr <tjdgkr7724@naver.com>
2026-03-24 20:05:04 +0900

    세팅 변경 5차

--- Back/myproject/myproject/settings.py
+++ Back/myproject/myproject/settings.py
@@ -17,12 +17,9 @@ REFRESH_TOKEN_HASH_SECRET = env('REFRESH_TOKEN_HASH_SECRET')
 DEBUG = env('DEBUG')
 
 DEBUG = True
-ALLOWED_HOSTS = [
-    "127.0.0.1",
-    "localhost",
-    "teamproject-v2qg.onrender.com",
-    "teamproject-odpg.onrender.com",
-]
+
+ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=["127.0.0.1", "localhost"])
+
 INSTALLED_APPS = [
     "daphne",
     "django.contrib.auth",

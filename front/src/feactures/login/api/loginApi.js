4946a0c0852d578d49ab074de9ce048febd778ad
rlawotjs158 <rlawotjs158@naver.com>
2026-03-24 23:27:10 +0900

    2차 변수 제거

--- front/src/feactures/login/api/loginApi.js
+++ front/src/feactures/login/api/loginApi.js
@@ -1,7 +1,4 @@
 // src/login/api/loginApi.js
-const API_BASE = process.env.BACK_APP_API_BASE_URL;
-
-
 export const adminLoginAPI = async (id, password, admin_code) => {
   const response = await fetch(`/api/check_admin_login/`, {
     method: "POST",
